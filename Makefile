b0c663bf7a346f173074132662ace86b4540d1f5
Lukas Retschmeier <lukas.retschmeier@tum.de>
2022-10-11 11:22:29 +0200

    Adding VTT skeleton

--- Makefile
+++ Makefile
@@ -5,5 +5,9 @@ venv/bin/activate: requirements.txt
 run: venv/bin/activate
 	./venv/bin/python3 ./scripts/gag.py
 
-run-demo: venv/bin/activate
+run/demo: venv/bin/activate
 	./venv/bin/python3 ./scripts/gag-analyse-vtt.py -path "./samples/" -regex "*.mp3"
+
+# Bad results, but useful for testing the script when no GPU is available
+run/demo/fast: venv/bin/activate
+	./venv/bin/python3 ./scripts/gag-analyse-vtt.py -path "./samples/" -regex "*.mp3" -model "tiny"
--- scripts/gag-analyse-vtt.py
+++ scripts/gag-analyse-vtt.py
@@ -1,6 +1,8 @@
 """Analysising a folder containing audio files and writing the transcription into the same folder/out
 """
 from whisper.utils import write_vtt
+import webvtt
+from pyannote.audio import Pipeline
 import whisper, os, sys, argparse, logging
 from pathlib import Path
 
@@ -16,8 +18,9 @@ parser.add_argument('-path', dest='path', type=str,
                     help='path to search')
 args = parser.parse_args()
 
-# Loads the desired model
+# Loads the desired models
 model = whisper.load_model(args.model)
+pipeline = Pipeline.from_pretrained('pyannote/speaker-diarization') # Speaker recognizition
 
 # Sets the path to the file to be converted 
 paths = Path(args.path).glob(args.regex)
@@ -30,7 +33,22 @@ for filename in paths:
         logging.debug('Operating on' + str(filename))
         # Start whisper VTT
         result = model.transcribe(str(filename) ,verbose=True)
+        # TODO own function
+        dz = pipeline(str(filename), min_speakers=2, max_speakers=7) 
+        dzList= list(dz.itertracks(yield_label=True))
 
-        with open(os.path.join(filename + ".vtt"), "w", encoding="utf-8") as vtt:
+        with open(os.path.join(filename,  ".vtt"), "w", encoding="utf-8") as vtt:
                 write_vtt(result["segments"], file=vtt)
                 print(result["text"])
+
+def time(timeStr):
+  spl = timeStr.split(":")
+  time = int(spl[0]) * 60 * 60 + int(spl[1]) * 60 + float(spl[2]) 
+  return time
+
+def test():
+        captions = []
+        for caption in webvtt.read('lecun1.wav.vtt'):
+                captions.append([time(caption.start), time(caption.end), caption.start, caption.text])
+
+#Add void spans def
